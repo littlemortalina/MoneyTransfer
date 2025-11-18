a65ffed1da77202c85b2f902a47498ac7daf15c2
Alina <al89056667426@gmail.com>
2025-11-18 10:04:55 +0300

    Corrected

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MoneyTransferTest {
     @Test
-    void shouldTransferMoneyFromFirrstTpSecondCard() {
+    void shouldTransferMoneyFromSecondToFirstCard() {
         open("http://localhost:9999");
 
         var loginPage = new LoginPage();
